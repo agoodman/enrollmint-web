36a78a05d15eb55d1a293806dd610831874c3607
Aubrey Goodman <aubrey.goodman@gmail.com>
2011-07-04 01:38:32 -0400

    added post-back behavior as after save filter on subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,3 +1,5 @@
+require 'net/http'
+
 class Subscription < ActiveRecord::Base
 
   belongs_to :customer
@@ -8,11 +10,22 @@ class Subscription < ActiveRecord::Base
   attr_accessible :product_id, :expires_on
   
   before_create :generate_secret_key
+  after_save :synchronous_post_back
   
   # private
   
   def generate_secret_key
     self.secret_key = Digest::SHA1.hexdigest("--#{product.identifier}--#{customer.email}--")[0..9]
+  end
+
+  def synchronous_post_back
+    unless product.app.post_back_url.blank?
+      uri = URI.parse(product.app.post_back_url)
+      http = Net::HTTP.new(uri.host, uri.port)
+      request = Net::HTTP::Post.new(uri.request_uri, initheader = {'Content-Type' => 'application/json'})
+      request.body = [id].to_json
+      response = http.request(request)
+    end
   end
   
 end
