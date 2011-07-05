26309a858eb28d7b5957881e1aad8594d4f1dd76
Aubrey Goodman <aubrey.goodman@gmail.com>
2011-07-05 19:18:49 -0400

    changed transaction_id from integer to string

--- db/migrate/20110705211438_add_fields_to_receipts.rb
+++ db/migrate/20110705211438_add_fields_to_receipts.rb
@@ -3,18 +3,18 @@ class AddFieldsToReceipts < ActiveRecord::Migration
   def self.up
     add_column :receipts, :quantity, :integer
     add_column :receipts, :product_id, :integer
-    add_column :receipts, :transaction_id, :integer, :limit => 8
+    add_column :receipts, :transaction_id, :string
     add_column :receipts, :purchase_date, :timestamp
     add_column :receipts, :expiration_date, :timestamp
     remove_column :receipts, :receipt_data
   end
 
   def self.down
-    remove_column :receipts, :quantity, :integer
-    remove_column :receipts, :product_id, :integer
-    remove_column :receipts, :transaction_id, :integer
-    remove_column :receipts, :purchase_date, :timestamp
-    remove_column :receipts, :expiration_date, :timestamp
+    remove_column :receipts, :quantity
+    remove_column :receipts, :product_id
+    remove_column :receipts, :transaction_id
+    remove_column :receipts, :purchase_date
+    remove_column :receipts, :expiration_date
     add_column :receipts, :receipt_data, :text
   end
 
