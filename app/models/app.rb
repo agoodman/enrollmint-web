87826ca43dce44bcb814841906ec7c60d5451361
Aubrey Goodman <aubrey.goodman@gmail.com>
2011-07-07 17:52:58 -0400

    added user_id as accessible to fix a failing migrationn

--- app/models/app.rb
+++ app/models/app.rb
@@ -6,6 +6,6 @@ class App < ActiveRecord::Base
   validates_presence_of :user_id, :title, :bundle_identifier
   validates_uniqueness_of :bundle_identifier
   
-  attr_accessible :title, :bundle_identifier
+  attr_accessible :user_id, :title, :bundle_identifier
 
 end
