6c88aa35ff79fcafdba35d1a8f46df66c9e6a6ad
Aubrey <aubrey.goodman@gmail.com>
2012-01-24 22:21:15 -0500

    added dependent destroy directive to Customer.subscriptions

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,7 @@
 class Customer < ActiveRecord::Base
 
   belongs_to :user
-  has_many :subscriptions
+  has_many :subscriptions, :dependent => :destroy
   has_many :products, :through => :subscriptions
   
   validates_presence_of :user_id, :email
