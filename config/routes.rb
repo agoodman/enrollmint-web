086dfbfb1a4a2713cacfa1cd9287c5d02d6858de
Aubrey <aubrey.goodman@gmail.com>
2012-01-24 20:45:52 -0500

    turns out it was the missing get show mapping for secret key

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ EnrollMint::Application.routes.draw do
     match 'customers/:secret_key/receipts' => 'customers/receipts#create', :via => :post, :secret_key => SECRET_KEY_REGEX
     match 'subscriptions/:secret_key' => 'subscriptions#show', :via => :get, :secret_key => SECRET_KEY_REGEX
     match 'subscriptions/:secret_key' => 'subscriptions#update', :via => :put, :secret_key => SECRET_KEY_REGEX
+    match 'products/:secret_key' => 'products#show', :via => :get, :secret_key => SECRET_KEY_REGEX
     match 'products/:secret_key' => 'products#update', :via => :put, :secret_key => SECRET_KEY_REGEX
   end
 
