885a920fe29c1928a323ed5d2393a103920ab752
Aubrey <aubrey.goodman@gmail.com>
2012-01-24 21:08:26 -0500

    added id to customers#show contents

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -34,8 +34,8 @@ class CustomersController < ApplicationController
   def show
     respond_to do |format|
       format.html
-      format.json { render :json => @customer.to_json(:only => :email, :include => { :subscriptions => { :only => [ :expiration_date ], :methods => :product_identifier } }) }
-      format.xml { render :xml => @customer.to_xml(:only => :email, :include => { :subscriptions => { :only => [ :expiration_date ], :methods => :product_identifier } }) }
+      format.json { render :json => @customer.to_json(:only => [ :id, :email ], :include => { :subscriptions => { :only => [ :expiration_date ], :methods => :product_identifier } }) }
+      format.xml { render :xml => @customer.to_xml(:only => [ :id, :email ], :include => { :subscriptions => { :only => [ :expiration_date ], :methods => :product_identifier } }) }
     end
   end
   
